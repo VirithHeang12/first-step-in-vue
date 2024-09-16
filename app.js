b465fd85dc309735bacc2b4266e0badfa5e103a3
Heang Virith <virith.id@gmail.com>
2024-09-16 22:27:46 +0700

    init project

---
+++ app.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector('input');
+const buttonEl = document.querySelector('button');
+const ulEl = document.querySelector('ul');
+
+buttonEl.addEventListener('click', () => {
+  const liEl = document.createElement('li');
+  liEl.textContent = inputEl.value;
+  ulEl.appendChild(liEl);
+  inputEl.value = '';
+});
