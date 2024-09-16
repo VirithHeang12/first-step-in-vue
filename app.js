ecbc0bdd2ef82f8d186e7638c27a6f1d5ec92094
Heang Virith <virith.id@gmail.com>
2024-09-16 22:54:01 +0700

    Refactor code to use Vue.js for dynamic rendering and data binding

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const ulEl = document.querySelector('ul');
-
-buttonEl.addEventListener('click', () => {
-  const liEl = document.createElement('li');
-  liEl.textContent = inputEl.value;
-  ulEl.appendChild(liEl);
-  inputEl.value = '';
-});
+Vue.createApp({
+    data() {
+        return {
+            message: '',
+            input: '',
+            messages: []
+        }
+    },
+    methods: {
+        addElement() {
+            if (this.input === '') {
+                return;
+            }
+            this.messages.push(this.input);
+            this.input = '';
+        }
+    }
+}).mount('#app');
