03542ac4c69d69a212cd3393b603b2502dfeb938
Heang Virith <virith.id@gmail.com>
2024-09-20 23:20:35 +0700

    Refactor code to use Vue.js for dynamic rendering and data binding

--- app.js
+++ app.js
@@ -4,26 +4,35 @@ const app = Vue.createApp({
             input: '',
             messages: [],
             randomNumber: 0,
+            box1Active: false,
+            isActive: false,
+            error: false,
         }
     },
     methods: {
-        addElement(number, event) {
-            if (this.input === '') {
-                return;
-            }
-            this.messages.push(this.input);
-            this.input = '';
+        toggleActive() {
+            this.isActive = !this.isActive;
+        },
+        toggleError() {
+            this.error = !this.error;
         },
     },
     computed: {
         count() {
             return this.messages.length;
         },
+        classObject() {
+            return {
+                active: this.isActive && this.error,
+                'text-danger': this.error || this.isActive
+            }
+        }
     },
     watch: {
         count(newValue, oldValue) {
             console.log('count changed', newValue, oldValue);
         },
+        
     },
 });
 
